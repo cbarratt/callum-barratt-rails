1da2fb6ade5051797bd42282b449738ce17b00d0
Callum Barratt <callum@bolser.co.uk>
2015-04-12 16:13:49 +0100

    Amend serializer

--- app/controllers/api/v1/weighins_controller.rb
+++ app/controllers/api/v1/weighins_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::WeighinsController < Api::V1::BaseController
   def index
-    weighins = Weighin.order(taken_at: :desc)
+    weighins = Weighin.order(taken_at: :desc).first(7)
 
     render json: weighins
   end
--- app/serializers/weighin_serializer.rb
+++ app/serializers/weighin_serializer.rb
@@ -1,3 +1,7 @@
 class WeighinSerializer < ActiveModel::Serializer
   attributes :id, :weight, :bodyfat_mass, :bodyfat_percentage, :lean_mass, :taken_at
+
+  def taken_at
+    object.taken_at.strftime('%d %b %Y %I:%M %P')
+  end
 end
