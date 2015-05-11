e3f1c4adf2c993314d2c2c69db5ef5ac936c142c
Callum Barratt <callum@bolser.co.uk>
2015-05-11 15:17:17 +0100

    Added withings id to weighin JSON

--- app/serializers/weighin_serializer.rb
+++ app/serializers/weighin_serializer.rb
@@ -1,5 +1,5 @@
 class WeighinSerializer < ActiveModel::Serializer
-  attributes :id, :weight, :bodyfat_mass, :bodyfat_percentage, :lean_mass, :taken_at
+  attributes :id, :withings_id, :weight, :bodyfat_mass, :bodyfat_percentage, :lean_mass, :taken_at
 
   def taken_at
     object.taken_at.to_s(:weight)
